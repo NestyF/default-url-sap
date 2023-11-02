4752a50fc995dbf3aa926e72cadf229c47b6f0f7
NestyF <31319663+NestyF@users.noreply.github.com>
2023-11-02 09:23:07 -0500

    Update sap_request.py

--- sap_request.py
+++ sap_request.py
@@ -1,7 +1,10 @@
 import requests
 from termcolor import colored
 
-# Solicitar la ruta del archivo de texto al usuario
+# Solicitar la URL y puerto de SAP al usuario
+sap_url_port = input(colored("Introduce la URL y puerto de SAP (ejemplo: 10.10.10.10:8000): ", "blue"))
+
+# Solicitar la ruta del archivo de texto que contiene las URLs
 ruta_archivo = input(colored("Introduce la ruta del archivo de texto que contiene las URLs: ", "blue"))
 
 # Obtener la lista de URLs del archivo de texto
@@ -15,17 +18,19 @@ urls_correctas = []
 # Verificar cada URL en la lista
 for url in urls:
     try:
+        # Construir la URL completa con la URL y puerto de SAP
+        full_url = f"http://{sap_url_port}/{url}"
         # Enviar una solicitud HTTP GET a la URL
-        response = requests.get(url)
+        response = requests.get(full_url)
         # Comprobar el código de respuesta HTTP
         if response.status_code == 200:
-            print(colored(f"La URL {url} funciona correctamente.", "green"))
-            # Agregar la URL a la lista de URL correctas
-            urls_correctas.append(url)
+            print(colored(f"La URL {full_url} funciona correctamente.", "green"))
+            # Agregar la URL completa a la lista de URL correctas
+            urls_correctas.append(full_url)
         else:
-            print(colored(f"La URL {url} no funciona correctamente.", "red"))
+            print(colored(f"La URL {full_url} no funciona correctamente.", "red"))
     except requests.exceptions.RequestException:
-        print(colored(f"No se puede acceder a la URL {url}.", "red"))
+        print(colored(f"No se puede acceder a la URL {full_url}.", "red"))
 
 # Mostrar un resumen al final con las URL que funcionaron correctamente
 print("\nResumen de URLS que funcionaron correctamente:")
